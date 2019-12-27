057afccf2c5abe40b85e0aff210423562b123f6d
Rui Yin <yinsharp25@gmail.com>
2019-12-27 12:13:11 -0800

    Used conf file to store the credential instead of environment variables

--- main.go
+++ main.go
@@ -8,29 +8,34 @@ import (
 	"strings"
 	"time"
 	// "io/ioutil"
+	"github.com/magiconair/properties"
 	"net/http"
 	"net/smtp"
 	"os"
 )
 
 type Course struct {
-	dept string
-	course string
+	dept    string
+	course  string
 	section string
 }
 
 func main() {
-	if len(os.Args) != 4 {
-		fmt.Println("Usage:", os.Args[0], "DEPT", "COURSE", "SECTION")
+	if len(os.Args) != 5 {
+		fmt.Println("Usage:", os.Args[0], "DEPT", "COURSE", "SECTION", "RECEIVER EMAIL")
 		return
 	}
 	dept := strings.ToLower(os.Args[1])
 	course := strings.ToLower(os.Args[2])
 	section := strings.ToLower(os.Args[3])
-	temp := Course{dept:dept, course:course, section:section}
+	receiver := strings.ToLower(os.Args[4])
+	temp := Course{dept: dept, course: course, section: section}
+	file := "cc.conf"
+	p := properties.MustLoadFile(file, properties.UTF8)
+	pass := p.MustGetString("SENDERPASS")
 	for {
 		if getNumberofSeats(temp) != 0 {
-			notifyUser(temp)
+			notifyUser(temp, pass, receiver)
 			break
 		}
 		fmt.Printf("Currently there is no seat available for the course\n")
@@ -42,8 +47,8 @@ func main() {
 // returns the current general seats remaining for the section
 func getNumberofSeats(course Course) int {
 	temp := 0
-	url := "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&" + 
-	"dept=" + course.dept + "&course=" + course.course + "&section=" + course.section
+	url := "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&" +
+		"dept=" + course.dept + "&course=" + course.course + "&section=" + course.section
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -52,37 +57,35 @@ func getNumberofSeats(course Course) int {
 	if res.StatusCode != 200 {
 		log.Fatal("status code error: %s %d\n", res.Status, res.StatusCode)
 	}
-	doc,err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	doc.Find("table.\\'table").Each(func(index int, item *goquery.Selection) {
 		info := item.Text()
 		seats := strings.Split(info, "\n")
-		temp,_ = strconv.Atoi(strings.SplitAfter(seats[4], "General Seats Remaining:")[1])
+		temp, _ = strconv.Atoi(strings.SplitAfter(seats[4], "General Seats Remaining:")[1])
 	})
 	return temp
 }
 
-func notifyUser(course Course) {
+func notifyUser(course Course, pass string, receiver string) {
 	from := "ruisharp25@gmail.com"
-	pass := os.Getenv("SENDERPASS");
-	to := "yinsharp25@gmail.com"
 
 	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
+		"To: " + receiver + "\n" +
 		"Subject: Hello there from Course Checker \n\n" +
 		course.dept + course.course + " " + course.section +
 		" is available now, please register ASAP."
 
 	err := smtp.SendMail("smtp.gmail.com:587",
 		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-		from, []string{to}, []byte(msg))
+		from, []string{receiver}, []byte(msg))
 
 	if err != nil {
 		log.Printf("smtp error: %s", err)
 		return
 	}
-	
+
 	log.Print("email sent, please restart the program if you want to monitor the registration again\n")
 }
